eef5d93e0a71bbdea081973bed7beebb84a7d1bc
Ronak Surve <ronaksurve344rs@gmail.com>
2024-06-14 20:40:18 +0530

    multer file upload

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const path = require("path");
+const multer  = require('multer');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()} - ${file.originalname}`;
+      cb(null, fileName);
+    }
+})
+
+const upload = multer({ storage: storage })
+  
+app.use(express.urlencoded({extended : false}));
 
 app.get("/", (req,res) => {
     return res.render("home");
+});
+
+app.post("/upload", upload.single("profileImage"), (req,res) =>  {
+
+        console.log(req.file);
+        console.log(req.body);
+
+        return res.redirect("/");
 });
 
 app.listen(process.env.PORT, () => {console.log(`Server listening on PORT ${process.env.PORT}`)});
